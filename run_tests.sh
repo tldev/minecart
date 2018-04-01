c76ecccfced83a982bde5a891ec550f4745c44cf
Tom Johnell <tjohnell@gmail.com>
2018-04-01 14:36:24 -0400

    Re-org + colors

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,9 @@
 #!/usr/local/bin/bash
 
+blue='\033[0;34m'
+l_blue='\033[0;94m'
+no_color='\033[0m'
+
 if [ -d $HOME/.rbenv ]; then
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
@@ -9,6 +13,18 @@ spec_command="bundle exec spring rspec"
 test_command="bundle exec rails test"
 declare -A test_mapping=( ["spec"]="it|context|describe" ["test"]="test")
 declare -A run_mapping=( ["spec"]=${spec_command} ["test"]=${test_command} )
+
+main() {
+    if [[ $1 == *_spec.rb ]] || [[ $1 == *_test.rb ]]; then
+        run_tests $1 $2
+    else
+        find_related_file $1
+        if [[ ${related_file} != "" ]]; then
+            open_file_in_rubymine ${related_file}
+            run_tests ${related_file} 0
+        fi
+    fi
+}
 
 get_git_repo() {
     cd $(dirname "${1}")
@@ -22,10 +38,15 @@ get_project_folder() {
     project_folder=${git_repo}
 }
 
+get_local_path() {
+    get_project_folder
+    local_path=`echo $1 | sed "s|${project_folder}/||g"`
+}
+
 open_file_in_rubymine() {
     get_project_folder
     cmd="/Applications/RubyMine.app/Contents/MacOS/rubymine ${project_folder} --line 1 ${1}"
-    eval ${cmd}
+    eval ${cmd} > /dev/null
 }
 
 find_related_file() {
@@ -55,6 +76,8 @@ testable_line() {
 }
 
 run_tests() {
+    printf "\n${blue}Running tests:${no_color}\n"
+
     for type in "${!run_mapping[@]}"; do
         if [[ $1 == *_"$type".rb ]]; then
             if [[ ${type} == "test" ]]; then
@@ -62,13 +85,18 @@ run_tests() {
             fi
 
             testable_line ${type} $1 $2
+            get_local_path $1
             full_file=$1
+            full_local_path=${local_path}
             if [[ ${is_testable} == 1 ]]; then
                 full_file="$1:$2"
+                full_local_path="$local_path:$2"
             fi
 
             cmd="${run_mapping[$type]} ${full_file}"
-            echo $cmd
+            viz_cmd="${run_mapping[$type]} ${full_local_path}"
+
+            printf "\n\t${l_blue}${viz_cmd}${no_color}\n\n"
             eval $cmd
 
             if [[ ${type} == "spec" ]]; then
@@ -80,14 +108,4 @@ run_tests() {
     done
 }
 
-echo
-
-if [[ $1 == *_spec.rb ]] || [[ $1 == *_test.rb ]]; then
-    run_tests $1 $2
-else
-    find_related_file $1
-    if [[ ${related_file} != "" ]]; then
-        open_file_in_rubymine ${related_file}
-        run_tests ${related_file} 0
-    fi
-fi
+main $1 $2
